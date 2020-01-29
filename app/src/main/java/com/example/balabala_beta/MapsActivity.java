3e870654404c1c7e13818b50481cb814337edf62
Docta Rhyf <drrhyf@gmail.com>
2020-01-29 23:18:31 +0200

    Added login functionality p2

--- app/src/main/java/com/example/balabala_beta/MapsActivity.java
+++ app/src/main/java/com/example/balabala_beta/MapsActivity.java
@@ -6,8 +6,6 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 
-
-import android.content.res.Resources;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;

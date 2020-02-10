b6a6a854058c645ad1e12288b6cf1028e83dadec
Docta Rhyf <drrhyf@gmail.com>
2020-02-10 23:36:22 +0200

    Path to downloaded audio needs fixing

--- app/src/main/java/com/example/balabala_beta/ActivityMarkerDetails.java
+++ app/src/main/java/com/example/balabala_beta/ActivityMarkerDetails.java
@@ -1,19 +1,32 @@
 package com.example.balabala_beta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import java.io.File;
+import java.io.IOException;
 
 public class ActivityMarkerDetails extends AppCompatActivity {
 
     private static final String TAG = "AMD";
     private StorageReference storageRef = FirebaseStorage.getInstance().getReference();
-    StorageReference insecAudioRef;
+    StorageReference insecAudioRef = null;
+    private MediaPlayer player = null;
+    private String mInsecAudioFileName = null;
+    private File mLocalFile = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +38,59 @@ public class ActivityMarkerDetails extends AppCompatActivity {
 
 
 
-        String tag = getIntent().getExtras().getString("tag");
+        mInsecAudioFileName = getIntent().getExtras().getString("tag");
 
-        getSupportActionBar().setTitle(tag);
+        getSupportActionBar().setTitle(mInsecAudioFileName);
 
 
-        //insecAudioRef = storageRef.child("insec_audio/" + tag);
-        //Log.e(TAG, "onCreate: -> " + insecAudioRef.toString() );
-        Log.e(TAG, "onCreate: -> da ref : " + "insec_audio/" + tag );
+        Log.e(TAG, "onCreate: -> da ref : " + "insec_audio/" + mInsecAudioFileName );
 
+        insecAudioRef = storageRef.child("insec_audio/" + mInsecAudioFileName);
+
+        mLocalFile = null;
+        try {
+            mLocalFile = File.createTempFile(mInsecAudioFileName, "3pg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        insecAudioRef.getFile(mLocalFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                // Local temp file has been created
+                Log.e(TAG, "onSuccess: -> downloaded : " + mInsecAudioFileName );
+                playInsecAudio();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Log.e(TAG, "onFailure: -> " + exception.toString() );
+            }
+        });
+
+    }
+
+    private void playInsecAudio() {
+        player = new MediaPlayer();
+        try {
+
+
+            String file = this.getCacheDir() + mLocalFile.toString();
+
+            Log.e(TAG, "playInsecAudio: file -> " + file );
+            player.setDataSource(file);//(mLocalFile);
+            player.prepare();
+            player.start();
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    player.release();
+                    player = null;
+                }
+            });
+        } catch (IOException e) {
+            Log.e(TAG, "prepare() failed");
+        }
     }
 }
